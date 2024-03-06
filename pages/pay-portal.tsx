33e3188037b02cfa1f8fd826dd0b497dd86fc669
Evgeni Leonti <evgeni.leonti@unipaas.com>
2024-03-06 15:46:53 +0200

    pay portal update added

--- pages/pay-portal.tsx
+++ pages/pay-portal.tsx
@@ -16,14 +16,26 @@ export default function Home() {
         <h2 className="text-xl font-bold my-4">Pay Portal</h2>
         <div className="mt-4 flex items-baseline space-x-2">
           <div>
-            <Button onClick={() => {
-              const payPortal = components.create("payPortal", {
-                vendor: {
-                  name: "Vendor Name",
-                }
-              });
-              payPortal.mount("#payPortal");
-            }}>Create</Button>
+            <div className="flex items-center space-x-2">
+              <Button onClick={() => {
+                const payPortal = components.create("payPortal", {
+                  vendor: {
+                    name: "Vendor Name",
+                  }
+                });
+                payPortal.mount("#payPortal");
+              }}>Create</Button>
+              <Button onClick={() => {
+                const payPortal = components.create("payPortal", {
+                  vendor: {
+                    name: "Vendor Name 2",
+                  }
+                });
+                payPortal.mount("#payPortal");
+              }}>Update</Button>
+
+            </div>
+
             <br />
             <br />
             <div id="payPortal" className="w-full"/>
